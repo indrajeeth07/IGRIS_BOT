74a1464300df8d7af682123c4432205b6f021fe1
indrajeeth07 <78995380+indrajeeth07@users.noreply.github.com>
2021-02-13 21:01:21 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -51,7 +51,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hello {},My Name is JARVIS
+Hello {},My Name is JARVIS.
 𝐈 𝐀𝐌 𝐀 𝐑𝐎𝐘𝐀𝐋 𝐊𝐍𝐈𝐆𝐇𝐓 𝐅𝐑𝐎𝐌 𝐒𝐎𝐋𝐎 𝐋𝐄𝐕𝐄𝐋𝐈𝐍𝐆.
 I will help you manage your group.
 TO KNOW MY COMMANDS CLICK /help.
